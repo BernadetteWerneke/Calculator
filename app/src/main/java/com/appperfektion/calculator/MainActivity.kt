e9fee628edd680549fc19d5806e9ee7443a887e1
Bernadette Werneke <bernadettewerneke@gmail.com>
2024-07-13 19:07:47 +0200

    landscape screen, add the clicklistener to the numbers

--- app/src/main/java/com/appperfektion/calculator/MainActivity.kt
+++ app/src/main/java/com/appperfektion/calculator/MainActivity.kt
@@ -1,20 +1,108 @@
 package com.appperfektion.calculator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.appperfektion.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+    var number: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.textViewResult.text = "0"
+
+        mainBinding.btnZero.setOnClickListener {
+            onNumberClicked("0")
         }
+
+        mainBinding.btnOne.setOnClickListener {
+            onNumberClicked("1")
+        }
+
+        mainBinding.btnTwo.setOnClickListener {
+            onNumberClicked("2")
+        }
+
+        mainBinding.btnThree.setOnClickListener {
+            onNumberClicked("3")
+        }
+
+        mainBinding.btnFour.setOnClickListener {
+            onNumberClicked("4")
+        }
+
+        mainBinding.btnFive.setOnClickListener {
+            onNumberClicked("5")
+        }
+
+        mainBinding.btnSix.setOnClickListener {
+            onNumberClicked("6")
+        }
+
+        mainBinding.btnSeven.setOnClickListener {
+            onNumberClicked("7")
+        }
+
+        mainBinding.btnEight.setOnClickListener {
+            onNumberClicked("8")
+        }
+
+        mainBinding.btnNine.setOnClickListener {
+            onNumberClicked("9")
+        }
+
+
+        mainBinding.btnAC.setOnClickListener {
+
+        }
+
+        mainBinding.btnDel.setOnClickListener {
+
+        }
+
+        mainBinding.btnDivider.setOnClickListener {
+
+        }
+
+        mainBinding.btnMulti.setOnClickListener {
+
+        }
+
+        mainBinding.btnMinus.setOnClickListener {
+
+        }
+
+        mainBinding.btnPlus.setOnClickListener {
+
+        }
+
+        mainBinding.btnEqual.setOnClickListener {
+
+        }
+
+        mainBinding.btnDot.setOnClickListener {
+
+        }
+
+    }
+
+    private fun onNumberClicked(clickedNumber: String){
+        if (number == null){
+            number = clickedNumber
+        } else {
+            number += clickedNumber
+        }
+        mainBinding.textViewResult.text = number
+    }
+
+    fun plus(){
+
     }
 }
